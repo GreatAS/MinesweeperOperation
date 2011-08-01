97365d04e1fdc75f65989acb440374de58731c8f
GreatAS <akira.radiant@gmail.com>
2011-08-02 00:42:34 +0900

    Create eclipse project.

---
+++ src/main/java/jp/ac/waseda/cs/washi/greatas/Operator.java
@@ -0,0 +1,16 @@
+package jp.ac.waseda.cs.washi.greatas;
+
+public class Operator {
+	
+	//field
+	
+	//constructor
+	public Operator() {
+		
+	}
+	
+	//method
+	public void analysisCommand(String cmd) {
+		String[] arguments = cmd.split(" ");
+	}
+}
